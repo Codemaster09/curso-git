43850d608c901f45b572f64f433d5e4d79d9631f
Kylo <kylorencodes@gmail.com>
2023-03-06 10:48:27 +0100

    Render posts

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ const getPosts = async (user) => {
   return posts;
 }
 
+const renderHtml = (user, posts) => {
+  const content = document.getElementById('content');
+  content.innerHTML += `<h3>Posts del usuario ${user.email}</h3>`;
+  
+  posts.forEach(post => {
+    content.innerHTML += `
+    <div class="post">
+      <h4>${post.title}</h4>
+      <p>${post.body}</p>
+      <br>
+      ${post.comments.map(c => `<p><span>${c.email}:</span>${c.body}</p>`).join('')}
+    </div>
+    `;
+  })
+}
+
 const getCommentsForEachPost = async (posts) => {
   const res = await Promise.all(posts.map(post => 
     fetch(`${url}/comments?postId=${post.id}&_limit=2`)  
@@ -29,8 +45,7 @@ const getBlogContent = async () => {
     const posts = await getPosts(user);
     await getCommentsForEachPost(posts);
 
-    console.log(user);
-    console.log(posts);
+    renderHtml(user, posts);
   } catch (err) {
     console.log(err);
   }
